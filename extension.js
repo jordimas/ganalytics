a7adec826efe600f2292af74b12a8313a7c396e1
Jordi Mas <jmas@softcatala.org>
2019-08-24 21:25:54 +0200

    2 buttons call

--- extension.js
+++ extension.js
@@ -4,9 +4,10 @@ const Tweener = imports.ui.tweener;
 const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
 const Mainloop = imports.mainloop;
+const Soup = imports.gi.Soup
 
 
-let text, button, buttonText, box, timerid, visits, timer;
+let text, button, buttonText, buttonText2, box, timer1id, visits, timer1, timer2id, timer2;
 
 const GAnalytics = class GAnalytics
 {
@@ -38,20 +39,33 @@ const GAnalytics = class GAnalytics
     }
 
     constructor() {
-
         visits = 0;
     }
 
-    _update() {
+    _update1() {
         visits = ++visits;
         buttonText.set_text(visits.toString());
         return true;
     }
 
+    _update2() {
+
+        log("_update2");
+        return true;
+    }
+
+
     enable() {
 
         buttonText = new St.Label({
-                    text: "Loading...",
+                    style_class: 'label',
+                    text: "Button1",
+                    y_align: Clutter.ActorAlign.CENTER
+                });
+
+        buttonText2 = new St.Label({
+                    style_class: 'label',
+                    text: "Button2",
                     y_align: Clutter.ActorAlign.CENTER
                 });
 
@@ -68,18 +82,23 @@ const GAnalytics = class GAnalytics
         box = new St.BoxLayout({ style_class: 'panel-launcher-box'});
         box.add(button);
         box.add(buttonText);
+        box.add(buttonText2);
 
         button.set_child(icon);
         button.connect('button-press-event', this._showHello);
-
-        timer = 1000
-        timerid = Mainloop.timeout_add(timer, Lang.bind(this, this._update));
         Main.panel._rightBox.insert_child_at_index(box, 0);
+
+
+        timer1 = 1000
+        timer1id = Mainloop.timeout_add(timer1, Lang.bind(this, this._update1));
+        timer2 = 1000
+        timer2id = Mainloop.timeout_add(timer1, Lang.bind(this, this._update2));
+
     }
 
     disable() {
         Main.panel._rightBox.remove_child(box);
-        Mainloop.source_remove(timerid);
+        Mainloop.source_remove(timer1id);
     }
 };
 
