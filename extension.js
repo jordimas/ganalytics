c6e85ebabec3833e0818a9c96ad07dc4cf673788
Jordi Mas <jmas@softcatala.org>
2019-08-24 12:59:14 +0200

    With class

--- extension.js
+++ extension.js
@@ -1,75 +1,100 @@
-	
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 const Clutter = imports.gi.Clutter;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
 
-let text, button, buttonText, box;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
+let text, button, buttonText, box, timerid, visits, timer;
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
+const GAnalytics = class GAnalytics
+{
+
+    _hideHello() {
+        Main.uiGroup.remove_actor(text);
+        text = null;
     }
 
-    text.opacity = 255;
+    _showHello() {
+        if (!text) {
+            text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
+            Main.uiGroup.add_actor(text);
+        }
 
-    let monitor = Main.layoutManager.primaryMonitor;
+        text.opacity = 255;
 
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
+        let monitor = Main.layoutManager.primaryMonitor;
 
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2, 
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+        text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
+                          monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
 
-    update_data();
-    
-}
+        Tweener.addTween(text,
+                         { opacity: 0,
+                           time: 2, 
+                           transition: 'easeOutQuad',
+                           onComplete: _hideHello });
+
+    }
+
+    constructor() {
+
+        visits = 0;
+        buttonText = new St.Label({
+                    text: "Loading...",
+                    y_align: Clutter.ActorAlign.CENTER
+                });
+
+        button = new St.Bin({ style_class: 'panel-button',
+                              reactive: true,
+                              can_focus: true,
+                              x_fill: true,
+                              y_fill: false,
+                              track_hover: true });
+
+        let icon = new St.Icon({ icon_name: 'system-run-symbolic',
+                                 style_class: 'system-status-icon' });
+
+        box = new St.BoxLayout({ style_class: 'panel-launcher-box'});
+        box.add(button);
+        box.add(buttonText);
+
+
+        button.set_child(icon);
+        button.connect('button-press-event', this._showHello);
+
+        timer = 1000
+        timerid = Mainloop.timeout_add(timer, Lang.bind(this, this._update));
+    }
+
+    _update() {
+        visits = visits++;
+        buttonText.set_text(visits.toString());
+        return true;
+    }
+
+    enable() {
+        Main.panel._rightBox.insert_child_at_index(box, 0);
+    }
+
+    disable() {
+        Main.panel._rightBox.remove_child(box);
+        Mainloop.source_remove(timerid);
+    }
+};
+
+let _instance;
 
 function init() {
-
-    buttonText = new St.Label({
-                text: "Loading...",
-                y_align: Clutter.ActorAlign.CENTER
-            });
-
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
-
-    box = new St.BoxLayout({ style_class: 'panel-launcher-box'});
-    box.add(button);
-    box.add(buttonText);
-
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
-}
-
-
-function update_data() {
-
-    buttonText.set_text("100")
+    _instance = new GAnalytics();
+    return _instance;
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(box, 0);
+   _instance.enable();
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(box);
+   _instance.disable();
 }
+
