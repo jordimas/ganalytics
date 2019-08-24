cdcb1aadae0eb100968305be1f120f80baccdad8
Jordi Mas <jmas@softcatala.org>
2019-08-24 12:18:11 +0200

    Show text

--- extension.js
+++ extension.js
@@ -2,8 +2,9 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
+const Clutter = imports.gi.Clutter;
 
-let text, button;
+let text, button, buttonText, box;
 
 function _hideHello() {
     Main.uiGroup.remove_actor(text);
@@ -28,26 +29,47 @@ function _showHello() {
                        time: 2, 
                        transition: 'easeOutQuad',
                        onComplete: _hideHello });
+
+    update_data();
+    
 }
 
 function init() {
+
+    buttonText = new St.Label({
+                text: "Loading...",
+                y_align: Clutter.ActorAlign.CENTER
+            });
+
     button = new St.Bin({ style_class: 'panel-button',
                           reactive: true,
                           can_focus: true,
                           x_fill: true,
                           y_fill: false,
                           track_hover: true });
+
     let icon = new St.Icon({ icon_name: 'system-run-symbolic',
                              style_class: 'system-status-icon' });
+
+    box = new St.BoxLayout({ style_class: 'panel-launcher-box'});
+    box.add(button);
+    box.add(buttonText);
+
 
     button.set_child(icon);
     button.connect('button-press-event', _showHello);
 }
 
+
+function update_data() {
+
+    buttonText.set_text("100")
+}
+
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    Main.panel._rightBox.insert_child_at_index(box, 0);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    Main.panel._rightBox.remove_child(box);
 }
